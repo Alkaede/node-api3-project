adc37d75bb432740a941b15a407c72020750d363
Andrew Castillo <andrewcast32@gmail.com>
2021-05-11 20:25:41 -0700

    server runs, routing setting up

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,5 +1,7 @@
 function logger(req, res, next) {
   // DO YOUR MAGIC
+  console.log(`[${new Date().toISOString()}] ${req.method} from ${req.headers.host}${req.url}`);
+  next();
 }
 
 function validateUserId(req, res, next) {
@@ -15,3 +17,4 @@ function validatePost(req, res, next) {
 }
 
 // do not forget to expose these functions to other modules
+module.exports = logger, validateUser, validateUserId, validatePost
--- api/server.js
+++ api/server.js
@@ -1,13 +1,22 @@
 const express = require('express');
-
+const postsRouter = require('./router/posts-router');
 const server = express();
 
-// remember express by default cannot parse JSON in request bodies
+const logger = require('./middleware/middleware');
 
+// remember express by default cannot parse JSON in request bodies
+server.use(express.json());
 // global middlewares and the user's router need to be connected here
+server.use(logger);
+server.use('/api/posts', postsRouter);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
+
+server.use((req, res, next)=>{
+  res.status(404).send('<h1>Sorry no page for that here</h1>');
+  next();
+})
 
 module.exports = server;
